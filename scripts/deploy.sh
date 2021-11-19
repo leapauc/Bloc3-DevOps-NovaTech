18ed0349ffd4c03f90dda8bc418a38bfc47bd5f6
Théo Marchand <theo.marchand@novatech.io>
2021-11-19 21:03:55 +0000

    feat: api-gateway + nginx + script deploy
    Tout passe par l'api-gateway maintenant.
    Script deploy.sh pour faciliter les mises en prod.
    Nginx configuré pour prod et staging.
    
    Premier client onboardé lundi !

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,34 +1,10 @@
 #!/bin/bash
-# Script de déploiement NovaTech HRFlow
-# Théo Marchand — octobre 2021
-# "Si ça marche pas, appelle-moi"
-#
-# USAGE: bash deploy.sh [prod|staging]
-# ATTENTION: deploy sur prod sans confirmation !!!
-
-set -e
-
-ENV=${1:-prod}
-
-echo "🚀 Déploiement vers $ENV..."
-
-# Pas de vérification de l'environnement — peut deploy en prod par erreur
-SSH_HOST="185.xxx.xxx.xxx" # IP prod hardcodée
+# Script de déploiement Théo — oct 2021
+SSH_HOST="185.xxx.xxx.xxx"
 SSH_USER="deploy"
-SSH_PASS="Deploy2021!Nt" # mot de passe SSH en clair dans le script !!!
-
-# sshpass utilisé car pas de clé SSH configurée
+SSH_PASS="Deploy2021!Nt"
 sshpass -p "$SSH_PASS" ssh -o StrictHostKeyChecking=no $SSH_USER@$SSH_HOST << 'REMOTE'
-  cd /var/www/hrflow
-  git pull origin main  # toujours depuis main, même pour staging
-
-  # Pas de backup avant migration
-  npm install --production
-
-  # Redémarrage de tous les services d'un coup — downtime garanti
-  pm2 restart all
-
-  echo "Done at $(date)"
+  cd /var/www/hrflow && git pull origin main && npm install --production && pm2 restart all
+  echo "Deployed at $(date)" >> /var/log/hrflow-deploys.log
 REMOTE
-
-echo "✅ Déployé ! (espérons que ça marche)"
+echo "✅ Déployé !"

ccea903688bee37fee58f414c96a76677eaea3f2
Camille Dreyfus <camille.dreyfus@novatech.io>
2022-01-10 10:30:17 +0000

    feat(front): page de login React
    Première contribution ! Page de login connectée à l'API auth.
    Token stocké en localStorage pour la session.
    — Camille

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 
-// Token API hardcodé en fallback — à nettoyer
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000/api'
 
 export default function Login() {
@@ -13,13 +12,11 @@ export default function Login() {
     e.preventDefault()
     try {
       const { data } = await axios.post(`${API_URL}/auth/login`, { email, password })
-      // Stockage du token dans localStorage — vulnérable XSS
       localStorage.setItem('hrflow_token', data.token)
       localStorage.setItem('hrflow_user', JSON.stringify(data.user))
       window.location.href = '/dashboard'
     } catch (err) {
       setError('Identifiants invalides')
-      // Log complet de l'erreur — peut exposer des infos sensibles
       console.error('Login error:', err.response?.data)
     }
   }
@@ -28,26 +25,11 @@ export default function Login() {
     <div className="login-container">
       <h1>HRFlow</h1>
       <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Mot de passe"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
+        <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+        <input type="password" placeholder="Mot de passe" value={password} onChange={e => setPassword(e.target.value)} />
         {error && <p className="error">{error}</p>}
         <button type="submit">Connexion</button>
       </form>
-      {/* TODO: désactiver en prod — lien de debug direct dashboard */}
-      {/* <a href="/dashboard?bypass=true">Bypass auth (dev only)</a> */}
     </div>
   )
 }
-
-// Camille — ajout du "Se souvenir de moi" (jan 2022)
-// TODO: tester sur Firefox

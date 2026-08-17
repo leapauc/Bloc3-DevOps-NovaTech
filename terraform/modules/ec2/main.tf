8f9b7afde98d0c27f058654c7856eacbb3d1060d
leapauc <lea.pauchot@gmail.com>
2026-08-17 14:43:28 +0200

    ci: open to internet

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -109,7 +109,7 @@ resource "aws_security_group" "k3s" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # Grafana (NodePort)

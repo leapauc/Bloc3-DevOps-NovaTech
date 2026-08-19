42c83aeacc4503cc52b73db8913703e45434d8b9
leapauc <lea.pauchot@gmail.com>
2026-08-19 10:42:09 +0200

    ci: add comment to test e2e test pipeline update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './components/Login' 
 import Dashboard from './pages/Dashboard' 
 
+/**/
 export default function App() { 
   return ( 
     <BrowserRouter> 
